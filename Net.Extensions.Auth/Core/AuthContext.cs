0f3655b515728c6f87a10a1ad6c28842d99d21e8
José Miguel <j.ramos@jm-ramos.com>
2025-06-25 11:00:04 +0200

    Create AuthContext.cs

---
+++ Net.Extensions.Auth/Core/AuthContext.cs
@@ -0,0 +1,23 @@
+﻿using Net.Extensions.Auth.Interfaces;
+
+namespace Net.Extensions.Auth.Core
+{
+    public static class AuthContext
+    {
+        private static IAuthProvider? _provider;
+
+        public static void RegisterProvider(IAuthProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public static Task<AuthUser?> LoginAsync() =>
+            _provider?.LoginAsync() ?? Task.FromResult<AuthUser?>(null);
+
+        public static Task LogoutAsync() =>
+            _provider?.LogoutAsync() ?? Task.CompletedTask;
+
+        public static bool IsAuthenticated => _provider?.IsAuthenticated ?? false;
+        public static AuthUser? CurrentUser => _provider?.CurrentUser;
+    }
+}
