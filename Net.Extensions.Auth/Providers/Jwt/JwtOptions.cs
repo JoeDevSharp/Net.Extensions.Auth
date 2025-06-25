17c71a39cd3a839b3925d85447f14a729d930d1e
José Miguel <j.ramos@jm-ramos.com>
2025-06-25 11:00:21 +0200

    Create JwtOptions.cs

---
+++ Net.Extensions.Auth/Providers/Jwt/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Net.Extensions.Auth.Providers.Jwt
+{
+    public class JwtOptions
+    {
+        public string Token { get; set; } = "";
+        public string AuthorityPublicKey { get; set; } = ""; // Opcional: para validar firma
+        public bool ValidateExpiration { get; set; } = true;
+    }
+}
