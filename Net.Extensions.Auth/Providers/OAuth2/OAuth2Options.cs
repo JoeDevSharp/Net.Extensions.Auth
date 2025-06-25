1a6d1bd7d9096051c04e1a068ee32f8320daddb4
José Miguel <j.ramos@jm-ramos.com>
2025-06-25 11:00:25 +0200

    Create OAuth2Options.cs

---
+++ Net.Extensions.Auth/Providers/OAuth2/OAuth2Options.cs
@@ -0,0 +1,13 @@
+﻿namespace Net.Extensions.Auth.Providers.OAuth2
+{
+    public class OAuth2Options
+    {
+        public string ClientId { get; set; } = "";
+        public string ClientSecret { get; set; } = "";
+        public string RedirectUri { get; set; } = "";
+        public string AuthorizationEndpoint { get; set; } = "";
+        public string TokenEndpoint { get; set; } = "";
+        public string UserInfoEndpoint { get; set; } = "";
+        public string[] Scopes { get; set; } = Array.Empty<string>();
+    }
+}
