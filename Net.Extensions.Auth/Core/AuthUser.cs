abc043e4c9181de45180f4f52b7386db9e2971a0
José Miguel <j.ramos@jm-ramos.com>
2025-06-25 11:00:06 +0200

    Create AuthUser.cs

---
+++ Net.Extensions.Auth/Core/AuthUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Net.Extensions.Auth.Core
+{
+    public class AuthUser
+    {
+        public string Id { get; init; }
+        public string Username { get; init; }
+        public string Email { get; init; }
+        public IReadOnlyList<string> Roles { get; init; }
+        public IReadOnlyDictionary<string, string> Claims { get; init; }
+    }
+}
