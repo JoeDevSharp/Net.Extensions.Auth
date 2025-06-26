41c91f4ed9aeb61decb5a7ecebb5766c35c6fffb
José Miguel <j.ramos@jm-ramos.com>
2025-06-26 08:38:49 +0200

    Create IAuthProvider.cs

---
+++ Net.Extensions.OAuth2/Interfaces/IAuthProvider.cs
@@ -0,0 +1,12 @@
+﻿using Net.Extensions.Auth.Core;
+
+namespace Net.Extensions.Auth.Interfaces
+{
+    public interface IAuthProvider
+    {
+        Task<AuthUser?> LoginAsync();
+        Task LogoutAsync();
+        bool IsAuthenticated { get; }
+        AuthUser? CurrentUser { get; }
+    }
+}
