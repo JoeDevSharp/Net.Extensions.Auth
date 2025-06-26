7a5f70f770334080df730ad358057a33ff861579
José Miguel <j.ramos@jm-ramos.com>
2025-06-26 08:38:44 +0200

    Create AuthException.cs

---
+++ Net.Extensions.OAuth2/Exceptions/AuthException.cs
@@ -0,0 +1,8 @@
+﻿namespace Net.Extensions.Auth.Exceptions
+{
+    public class AuthException : Exception
+    {
+        public AuthException(string message) : base(message) { }
+        public AuthException(string message, Exception inner) : base(message, inner) { }
+    }
+}
