2fcf13e5497b6173181059048acd652f63b13e54
José Miguel <j.ramos@jm-ramos.com>
2025-06-26 08:38:47 +0200

    Create HttpClientExtensions.cs

---
+++ Net.Extensions.OAuth2/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Headers;
+
+namespace Net.Extensions.Auth.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static void UseAuthBearer(this HttpClient client, string token)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
